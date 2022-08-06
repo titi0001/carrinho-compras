ec1df42b56adb12cc66eb9eaace63ccad4150427
Pedro <pedro_hasse@yahoo.com.br>
2022-08-06 16:40:41 -0300

    :sparkles: Requisito 5 ok

---
+++ src/components/Items.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Items extends Component {
+  render() {
+    const { product } = this.props;
+    const { title, thumbnail, price } = product;
+    return (
+      <div data-testid="product">
+        <h3>{title}</h3>
+        <img src={ thumbnail } alt={ title } />
+        <span>
+          R$
+          {price}
+          ,00
+        </span>
+      </div>
+    );
+  }
+}
+
+Items.propTypes = {
+  product: PropTypes.shape.isRequired,
+};
+
+export default Items;
