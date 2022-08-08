4c6a8b1f7d90a67cac42776ad9ed515e9ba5bca6
Pedro <pedro_hasse@yahoo.com.br>
2022-08-08 14:18:25 -0300

    :sparkles: Requisito 8 em andamento

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -3,6 +3,22 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 class Items extends Component {
+  addToCart = () => {
+    const { product: { title, thumbnail, price } } = this.props;
+    const dataObj = { name: title, image: thumbnail, price };
+    const getStorage = localStorage.getItem('products');
+    const storage = [getStorage];
+    if (getStorage === null) {
+      localStorage.setItem('products', JSON.stringify(storage));
+    } else {
+      localStorage.setItem(
+        'products',
+        JSON.stringify([...JSON.parse(condition), dataObj,
+        ]),
+      );
+    }
+  }
+
   render() {
     const { product } = this.props;
     const { title, thumbnail, price, id } = product;
@@ -11,7 +27,7 @@ class Items extends Component {
         <h3>{title}</h3>
         <span>
           R$
-          {price}
+          {price.toFixed(2)}
         </span>
         <Link data-testid="product-detail-link" to={ `/ProductDetails/${id}` }>
           <img src={ thumbnail } alt={ title } />
@@ -21,14 +37,25 @@ class Items extends Component {
             Mais Detalhes
           </button>
         </Link>
-
+        <button
+          type="button"
+          data-testid="product-add-to-cart"
+          onClick={ () => this.addToCart() }
+        >
+          Adicionar ao carrinho
+        </button>
       </div>
     );
   }
 }
 
 Items.propTypes = {
-  product: PropTypes.string.isRequired,
+  product: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    thumbnail: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default Items;
