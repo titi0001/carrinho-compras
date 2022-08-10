318e2b142cb0c771f210831b9ec99878fe1d016b
Pedro <pedro_hasse@yahoo.com.br>
2022-08-10 18:00:36 -0300

    :sparkles: Requisito 10 quase lá timeeee

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -18,6 +18,29 @@ export default class Cart extends Component {
     this.setState({ storage: dataObj });
   }
 
+  handleIncrease = (id) => {
+    const { storage } = this.state;
+    storage.map((item) => {
+      if (item.id === id) {
+        item.quantity += 1;
+      }
+      return this
+        .setState({ storage: [...storage] }, () => localStorage
+          .setItem('products', JSON.stringify([...storage])));
+    });
+  }
+
+  handleDecrease = (id) => {
+    const { storage } = this.state;
+    storage.map((item) => {
+      if (item.id === id) {
+        item.quantity -= 1;
+      }
+      return this.setState({ storage: [...storage] }, () => localStorage
+        .setItem('products', JSON.stringify([...storage])));
+    });
+  }
+
   render() {
     const { storage } = this.state;
 
@@ -34,12 +57,35 @@ export default class Cart extends Component {
               >
                 {storage.reduce((acc, { quantity }) => acc + quantity, 0)}
               </p>
-              {storage.map(({ title, thumbnail, price, quantity }, index) => (
+              {storage.map(({ title, thumbnail, price, id, quantity }, index) => (
                 <div key={ index }>
                   <h3 data-testid="shopping-cart-product-name">{title}</h3>
                   <img src={ thumbnail } alt={ title } />
                   <span>{ (price * quantity).toFixed(2) }</span>
                   <p>{quantity}</p>
+                  <button
+                    type="button"
+                    data-testid="product-increase-quantity"
+                    onClick={ () => this.handleIncrease(id) }
+                    value={ id }
+                  >
+                    +
+                  </button>
+                  <button
+                    type="button"
+                    data-testid="product-decrease-quantity"
+                    disabled={ quantity === 1 }
+                    onClick={ () => this.handleDecrease(id) }
+                    value={ id }
+                  >
+                    -
+                  </button>
+                  <button
+                    type="button"
+                    data-testid="remove-product"
+                  >
+                    Remover do carrinho
+                  </button>
                 </div>
               ))}
             </div>
